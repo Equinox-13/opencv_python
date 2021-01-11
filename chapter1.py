1bbca5d465709501cdc22ced6cafc8b3576e6064
Equinox-13 <quaid.johar33@gmail.com>
2021-01-11 23:11:12 +0530

    chapter1: Added code to read image and display it in a window.

--- chapter1.py
+++ chapter1.py
@@ -1,3 +1,13 @@
 import cv2
 
-print("package imported")
+# file path of image
+path = '/home/bista/practice/opencv_projects/opencv_python/resources/cat.jpeg'
+
+# Reads image
+img = cv2.imread(path)
+
+# Display image in a window
+cv2.imshow("Output",img)
+
+# Halts image window
+cv2.waitKey(0)
